c2ba673475fb4ee3d2fdc19a5fba05e07b3f2aa5
creinis <115181527+creinis@users.noreply.github.com>
2024-08-01 10:44:47 +0200

    feat: create your fourth pattern to match any non-alphanumeric character. Combine the a-z, A-Z, and 0-9 ranges into a single character class and add a ^ as the first character  to negate the pattern

--- lecture.py
+++ lecture.py
@@ -396,5 +396,22 @@ constraints = [
     (special_chars, r'')
 ]        
 
+# Step 47
+
+# In a character class, you can combine multiple ranges by writing one range after another inside 
+# the square brackets (without any additional characters). 
+# For example: [a-d3-6] is the combination of [a-d] and [3-6].
+
+# Now, create your fourth pattern to match any non-alphanumeric character. 
+# Combine the a-z, A-Z, and 0-9 ranges into a single character class and add a ^ as the first character 
+# to negate the pattern.
+
+constraints = [
+    (nums, r'\d'),
+    (lowercase, r'[a-z]'),
+    (uppercase, r'[A-Z]'),
+    (special_chars, r'[^a-zA-Z0-9]')
+]    
+
 
 
