564512a21d975d145c27146bd5e0f27aa26b3454
creinis <115181527+creinis@users.noreply.github.com>
2024-08-01 10:42:22 +0200

    feat: Use the special_chars parameter as the first item and an empty string as the second item

--- lecture.py
+++ lecture.py
@@ -345,4 +345,15 @@ constraints = [
 (nums, '[0-9]'),(lowercase, '[a-z]')
 ]        
 
+# Step 41
+
+# Add one last tuple to your list. 
+# Use the special_chars parameter as the first item and an empty string as the second item.
+
+constraints = [
+    (nums, '[0-9]'),
+    (lowercase, '[a-z]'),
+    (uppercase, '[A-Z]'),
+    (special_chars, '')
+]        
 
