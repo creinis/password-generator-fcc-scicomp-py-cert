59ddd1135177522bb4ac0c64e8da824c21fd0be6
creinis <115181527+creinis@users.noreply.github.com>
2024-08-01 10:44:08 +0200

    feat: turn the four patterns from the constraints list into raw strings

--- lecture.py
+++ lecture.py
@@ -385,4 +385,16 @@ pattern = '.+'
 
 pattern = r'\.'
 
+# Step 45
+
+# Now, turn the four patterns from the constraints list into raw strings.
+
+constraints = [
+    (nums, r'[0-9]'),
+    (lowercase, r'[a-z]'),
+    (uppercase, r'[A-Z]'),
+    (special_chars, r'')
+]        
+
+
 
