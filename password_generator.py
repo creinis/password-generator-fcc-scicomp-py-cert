d7b134c0e56fe6a5acaf8e8a92a43e14a15f2522
creinis <115181527+creinis@users.noreply.github.com>
2024-08-01 10:58:35 +0200

    feat: Combine all characters into all_characters variable

--- password_generator.py
+++ password_generator.py
@@ -11,5 +11,7 @@ def generate_password(length=20, nums=1, special_chars=1, uppercase=1, lowercase
     digits = string.digits
     symbols = string.punctuation
     
+    # Combine all characters
+    all_characters = letters + digits + symbols
     
     return password
