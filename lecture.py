54d35426109bb09cdef71c8055579f707b1bffa5
creinis <115181527+creinis@users.noreply.github.com>
2024-08-01 10:09:03 +0200

    feat: import random module and try it

--- lecture.py
+++ lecture.py
@@ -60,4 +60,22 @@ all_characters = letters + digits + symbols
 print(all_characters)
 # Output: abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~
 
+# Step 8
+
+#It is a common convention to place import statements at the top of your code. 
+# And additionally, in case of multiple import statements, sort them in alphabetical order to improve readability.
+
+#At the top of your code, import the random module.
+
+import random
+
+# Step 9
+
+# The random module contains a pseudo-random number generator. 
+# Most of its functionalities depend on the random() function, 
+# which returns a floating point number in the range between 0.0 (inclusive) and 1.0 (exclusive).
+
+# Call the random() function and print the result.
+
+print(random.random())
 
