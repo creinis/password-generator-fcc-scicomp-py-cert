0a1648883a25ae2ae9bb021c4608ee38884a5e47
creinis <115181527+creinis@users.noreply.github.com>
2024-08-01 10:11:55 +0200

    feat: import secret modulo. for cryptographic purposes in not good to use random module.

--- lecture.py
+++ lecture.py
@@ -86,3 +86,29 @@ print(random.random())
 
 print(random.choice(all_characters))
 
+# Step 11
+
+# Every time the code runs, you should see a random character from the all_characters string. 
+# This is exactly what you want to achieve to create a random password.
+
+# However, the algorithm on which random relies makes the generated pseudo-random numbers predictable. 
+# Therefore, although the random module is suitable for the most common applications, 
+# it cannot be used for cryptographic purposes, due to its deterministic nature.
+
+# Instead of importing random, import the secrets module. 
+# Then change the print() call to use secrets.choice(all_characters).
+
+import secrets
+import string
+
+
+# Define the possible characters for the password
+letters = string.ascii_letters
+digits = string.digits
+symbols = string.punctuation
+
+# Combine all characters
+all_characters = letters + digits + symbols
+
+print(all_characters)
+print(secrets.choice(all_characters))
