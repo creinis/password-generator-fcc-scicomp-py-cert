139d837ddd4926a8b0e500d7048545500de7eff2
creinis <115181527+creinis@users.noreply.github.com>
2024-08-01 10:21:20 +0200

    feat: password variable declaration and the following for loop inside a while loop. True as the condition for your new loop

--- lecture.py
+++ lecture.py
@@ -192,3 +192,13 @@ new_password = generate_password(8)
 
 def generate_password(length, nums, special_chars, uppercase, lowercase):
 
+# Step 24
+
+# Put your password variable declaration and the following for loop inside a while loop. 
+# Use True as the condition for your new loop.
+
+    while True:
+        password = ''
+        # Generate password
+        for _ in range(length):
+            password += secrets.choice(all_characters)
